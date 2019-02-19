de1fca29eb8adf3cb7d2e3ac9c59ff49eced3aae
yutingLei <lyt_930617@163.com>
2019-02-19 14:11:26 +0800

    common icons

---
+++ iOS-Elements/Basic/Icons/ELIcons.swift
@@ -0,0 +1,60 @@
+//
+//  ELIcons.swift
+//  iOS-Elements
+//
+//  Created by conjur on 2019/2/19.
+//  定义常用图标
+//
+
+import UIKit
+
+public class ELIcons: NSObject {
+    /// icon资源文件地址
+    static let iconsPath = Bundle(for: ELIcons.self).path(forResource: "ELResources", ofType: "bundle")
+
+    /// 定义所有icons类型
+    public enum ELIconTypes: String {
+        case info = "el-icon-info.png"
+    }
+
+    /// 获取icon图片
+    ///
+    /// - Parameters:
+    ///   - type: icon图片所属类型，大小为64x64
+    /// - Returns: 获取的图片
+    public class func get(_ type: ELIconTypes, tintColor: UIColor? = nil) -> UIImage? {
+        guard let iconsPath = iconsPath else { return nil }
+        let iconFilePath = URL(fileURLWithPath: iconsPath + "/" + type.rawValue)
+        do {
+            let iconData = try Data(contentsOf: iconFilePath)
+            var icon = UIImage(data: iconData)
+            if let tintColor = tintColor {
+                icon = icon?.withTintColor(tintColor)
+            }
+            return icon
+        } catch {
+            return nil
+        }
+    }
+}
+
+/// 扩展UIImage方法
+extension UIImage {
+    func withTintColor(_ color: UIColor) -> UIImage? {
+        /// Begin context
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+
+        /// set fill color
+        color.setFill()
+        let rect = CGRect(origin: CGPoint.zero, size: size)
+
+        /// fill color
+        UIRectFill(rect)
+
+        /// Draw image
+        draw(in: rect, blendMode: .destinationIn, alpha: 1)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
+    }
+}
